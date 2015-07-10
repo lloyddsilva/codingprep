572d4032e939f932f442e3c7ec12cb1e9a98bb1a
Lloyd D'Silva <lloyddsilva@gmail.com>
2015-07-10 04:29:38 -0700

    leetcode: reverse binary tree

---
+++ InvertBinaryTree/src/edu/cmu/lloyddsilva/Driver.java
@@ -0,0 +1,8 @@
+package edu.cmu.lloyddsilva;
+
+public class Driver {
+	public static void main(String[] args) {
+		BinaryTree tree = new BinaryTree();
+		tree.invertTree(null);
+	}
+}
---
+++ InvertBinaryTree/src/edu/cmu/lloyddsilva/TreeNode.java
@@ -0,0 +1,8 @@
+package edu.cmu.lloyddsilva;
+
+public class TreeNode {
+	int val;
+	TreeNode left;
+	TreeNode right;
+	TreeNode(int x) { val = x; }
+}
