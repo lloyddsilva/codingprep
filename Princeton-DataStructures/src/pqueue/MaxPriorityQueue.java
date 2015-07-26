17debbaebd0205b41bd2436da050e6734c93bd92
Lloyd D'Silva <lloyddsilva@gmail.com>
2015-07-25 18:36:36 -0700

    max priority queue

--- Princeton-DataStructures/src/pqueue/MaxPriorityQueue.java
+++ Princeton-DataStructures/src/pqueue/MaxPriorityQueue.java
@@ -1,23 +1,28 @@
 package pqueue;
 
+import java.util.Collections;
 import java.util.PriorityQueue;
 import java.util.Scanner;
 
-public class DriverMinPQ {
+public class MaxPriorityQueue {
 	public static final int MAX_SIZE = 5;
 	
 	public static void main(String[] args) {
 		
-		PriorityQueue<Integer> pqueue = new PriorityQueue<Integer>();
+		PriorityQueue<Integer> pqueue = new PriorityQueue<Integer>(10, Collections.reverseOrder());
 
 		Scanner in = new Scanner(System.in);
-		while(in.hasNext()) {
+		int counter =0;
+		while(in.hasNext() && counter <10) {
 			int input = in.nextInt();
 			pqueue.add(input);
 			
 			if(pqueue.size() > MAX_SIZE) {
 				int test = pqueue.poll();
 			}
+			counter++;
 		}
+		
+		System.out.println(pqueue);
 	}
 }
