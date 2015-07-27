6e812f3fd3195f5aea52f1839497dc424e0e3b5b
Lloyd D'Silva <lloyddsilva@gmail.com>
2015-07-27 12:15:37 -0700

    look and say sequence

--- LookAndSay/src/edu/cmu/lloyddsilva/LookAndSay.java
+++ LookAndSay/src/edu/cmu/lloyddsilva/LookAndSay.java
@@ -1,5 +1,32 @@
 package edu.cmu.lloyddsilva;
 
 public class LookAndSay {
-
+	public static void main(String[] args) {
+		String num = "1";
+		
+		for(int i=1; i<=10; i++) {
+			System.out.println(num);
+			num = lookAndSay(num);
+		}
+	}
+	
+	public static String lookAndSay(String number) {
+		StringBuilder result = new StringBuilder();
+		
+		char repeat = number.charAt(0);
+		number = number.substring(1) + " ";
+		int times = 1;
+		
+		for(char actual: number.toCharArray()) {
+			if(actual == repeat) {
+				times += 1;
+			} else {
+				result.append(times + "" + repeat);
+				times = 1;
+				repeat = actual;
+			}
+		}
+		
+		return result.toString();
+	}
 }
